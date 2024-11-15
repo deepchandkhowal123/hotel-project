0f0fc4ad81de16dba1dfe238adfa9438bce3dd46
deepchandkhowal123 <deepchandkhowal123@gmail.com>
2024-11-15 18:46:08 +0530

    Your commit message

---
+++ hotelproject.js
@@ -0,0 +1,14 @@
+// hotelproject.js
+
+function bookRoom(roomType) {
+    // Alert the user that the room has been booked
+    alert("You have successfully booked a " + roomType + "!");
+    
+    // Here you can add more functionality, such as:
+    // - Redirecting to a payment page
+    // - Storing booking information in a database
+    // - Sending a confirmation email, etc.
+    
+    // For demonstration, let's log the booking to the console
+    console.log("Booking made for: " + roomType);
+}
